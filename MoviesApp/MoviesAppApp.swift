ca72f882c16802d079c4eaf709280fa9320b6736
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-04-20 18:10:15 +0530

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Adarsh Shukla on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
