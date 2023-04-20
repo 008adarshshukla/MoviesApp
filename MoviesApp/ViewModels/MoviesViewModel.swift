b8d53f39752377c973f9f62fddd7a55d0230ddb0
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-04-20 23:06:22 +0530

    Configured api key

--- MoviesApp/ViewModels/MoviesViewModel.swift
+++ MoviesApp/ViewModels/MoviesViewModel.swift
@@ -12,7 +12,7 @@ class MoviesViewModel: ObservableObject {
     
     @Published var moviesResult: [MoviesModel] = []
     
-    let apiKey = "34c902e6393dc8d970be5340928602a7"
+    let apiKey = "your api key"
     let imageUrlPrefix = "https://image.tmdb.org/t/p/w500"
     
     
